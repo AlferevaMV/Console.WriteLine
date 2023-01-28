a817061400a97087bc5f44d2662acbc5494633d5
Marina Alfereva <alferevamv@mail.ru>
2023-01-28 23:47:50 +0300

    add Ex 005 Condition If-Else

---
+++ Ex005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if (username == "Marina")
+{
+    Console.WriteLine("Ура, это же Marina!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
+
+
